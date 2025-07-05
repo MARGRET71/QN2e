430fc7a22408a5b723b08ed77cfc7938cc7da683
MARGRET71 <donsannie673@gmail.com>
2025-07-05 14:07:01 +0300

    Add files via upload

---
+++ NLU.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package bit.nlu;
+
+/**
+ *
+ * @author user
+ */
+public class NLU {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
